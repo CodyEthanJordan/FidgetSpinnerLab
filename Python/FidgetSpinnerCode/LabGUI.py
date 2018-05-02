185138438cba1538319fcf6c21b047328e287009
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2018-05-02 15:04:54 -0400

    Update ui and analyzis with sampleData

--- Python/FidgetSpinnerCode/LabGUI.py
+++ Python/FidgetSpinnerCode/LabGUI.py
@@ -5,12 +5,16 @@ Code hosted at https://github.com/CodyEthanJordan/FidgetSpinnerLab
 '''
 
 import wx
+from wx.lib.masked import NumCtrl
 import numpy as np
 import matplotlib
 matplotlib.use('WXAgg')
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 from matplotlib.backends.backend_wx import NavigationToolbar2Wx
 from matplotlib.figure import Figure
+
+# expected to be in same folder
+import analyzeData
 
 class LabGUI(wx.Frame):
     def __init__(self, *args, **kwargs):
@@ -30,24 +34,48 @@ class LabGUI(wx.Frame):
 
         self.chooseDataButton = wx.Button(panel, label="Choose Data File")
         self.chooseDataButton.Bind(wx.EVT_BUTTON, self.OnChooseFile)
-        self.dataFileName = wx.StaticText(panel, wx.ID_ANY, label="No File Chosen", style=wx.ALIGN_CENTER)
+        self.dataFileName = wx.StaticText(panel, wx.ID_ANY, label="No Data File Chosen", style=wx.ALIGN_CENTER)
 
         self.figure = Figure()
         self.axes = self.figure.add_subplot(111)
-        self.canvas = FigureCanvas(self, -1, self.figure)
+        self.canvas = FigureCanvas(panel, -1, self.figure)
+
+        hbox1 = wx.BoxSizer(wx.HORIZONTAL) 
+        l1 = wx.StaticText(panel, -1, "Data Start") 
+        hbox1.Add(l1, 1, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL,5) 
+        self.dataStartText = NumCtrl(panel) 
+        self.dataStartText.Disable()
+        hbox1.Add(self.dataStartText,1,wx.EXPAND | wx.ALIGN_LEFT | wx.ALL,5) 
+
+        self.runAnalysisButton = wx.Button(panel, label="Run Analysis")
+        self.runAnalysisButton.Bind(wx.EVT_BUTTON, self.OnRunAnalysis)
+        self.runAnalysisButton.Disable()
 
         vbox = wx.BoxSizer(wx.VERTICAL)
         vbox.Add(self.chooseDataButton)
         vbox.Add(self.dataFileName) 
-        vbox.Add(self.canvas)
+        vbox.Add(hbox1) 
+        vbox.Add(self.runAnalysisButton)
+        vbox.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
         panel.SetSizer(vbox) 
-        self.Fit()
 
-        self.SetTitle('Simple menu')
+        self.SetTitle('Analyzing Slowdown of Spinner')
+        self.SetSize(640,480)
         self.Centre()
 
     def OnShowAbout(self, e):
         wx.MessageBox(info, 'About', wx.OK | wx.ICON_INFORMATION)
+
+    def OnRunAnalysis(self, e):
+        dataStart = int(self.dataStartText.GetValue())
+        velocity = analyzeData.findVelocity(self.data[dataStart:])
+        self.axes.clear()
+        self.axes.plot(velocity)
+        self.axes.relim()
+        self.axes.autoscale_view()
+        self.figure.canvas.draw()
+        self.figure.canvas.flush_events()
+
 
     def OnChooseFile(self, e):
         with wx.FileDialog(self, "Open XYZ file",
@@ -59,12 +87,16 @@ class LabGUI(wx.Frame):
             # Proceed loading the file chosen by the user
             pathname = fileDialog.GetPath()
             try:
-                data = np.loadtxt(pathname)
-                self.axes.plot(data)
+                self.dataFileName.SetLabel('Loading file')
+                self.data = np.loadtxt(pathname)
+                self.axes.plot(self.data)
                 self.axes.relim()
                 self.axes.autoscale_view()
                 self.figure.canvas.draw()
                 self.figure.canvas.flush_events()
+                self.dataStartText.Enable()
+                self.runAnalysisButton.Enable()
+                self.dataFileName.SetLabel(pathname)
             except IOError:
                 wx.LogError("Cannot open file '%s'." % newfile)
 
--- Python/FidgetSpinnerCode/analyzeData.py
+++ Python/FidgetSpinnerCode/analyzeData.py
@@ -4,37 +4,36 @@ from collections import deque
 import scipy.optimize as opt
 import argparse
 
+def findVelocity(data):
+    crossings = []
+    zeroPoint = np.mean(data)
+    crossings = np.where(np.diff(np.sign(data-zeroPoint)))[0]
+    period = np.diff(crossings)
+    filteredPeriod = period[np.append(np.diff(period) < 4000, True)]
+    velocity = 1 / filteredPeriod
+    filteredVelocity = velocity[velocity < np.mean(velocity)]
+    return filteredVelocity
+
+
 def analyzeData(filename):
     data = np.loadtxt(filename)
 
-    # %% Find zero crossings
-    crossings = []
-    zeroPoint = 700
-    lastPoint = data[0]
-    lastPositive = lastPoint > zeroPoint
-    i = 0
-    for d in data[1:]:
-        if lastPositive and d < zeroPoint:
-            x = (lastPoint - zeroPoint)/(d - lastPoint) + i
-            crossings.append(x) #update to lerp
-        lastPoint = d
-        lastPositive = lastPoint >= zeroPoint
-        i = i + 1
+    plt.ion()
 
-    crossings = np.array(crossings)
-    crossings2 = np.where(np.diff(np.sign(data-zeroPoint)))[0]
-    t = np.diff(crossings)
-    plt.plot(crossings, zeroPoint + 0*crossings, 'ro', crossings2, zeroPoint + 10+0*crossings2, 'bo', data)
-    plt.show()
+    plt.plot(data)
+    plt.draw()
+    plt.pause(.001)
+    
+    startData = input("Where does data start? ")
+    startData = int(startData)
+    data = data[startData:]
 
-    # %%
-    newT = t[np.append(np.diff(t) < 4000, True)]
-    linear = lambda x, a, b: a*x + b
-    x = np.array(list(range(len(newT))))
-    popt, r = opt.curve_fit(linear, x, 1/newT)
-    plt.plot(x,linear(x, *popt))
+    plt.close()
+    plt.ioff()
+    
+    velocity = findVelocity(data[startData:])
 
-    plt.plot(1/newT)
+    plt.plot(velocity)
     plt.show()
 
 
